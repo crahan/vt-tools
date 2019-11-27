1d08e321556c357ebe1be172cace84f1c7941137
Thomas Bouve <thomas.bouve@be.ibm.com>
2019-11-27 18:54:31 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    """Open files relative to package."""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def find_scripts():
+    scripts = []
+    exclude = ['setup.py']
+    for file in os.scandir('.'):
+        if file.name.endswith('.py') and file.is_file() and (file.name not in exclude):
+            scripts.append(file.name)
+    return scripts
+
+
+setup(
+    name='vt-tools',
+    version='0.1.0',
+    author="Thomas Bouve (@crahan)",
+    author_email="crahan@n00.be",
+    description='VirusTotal Python scripts.',
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    scripts=find_scripts(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+    ],
+    install_requires=[
+        'yamjam',
+    ],
+)
